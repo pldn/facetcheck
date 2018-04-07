67e2f2517e5433eed21e6f9e2df0dcf5514d968a
Laurens Rietveld <laurens.rietveld@gmail.com>
2018-04-07 21:18:15 +0200

    use same prefixes as bgt

--- src/prefixes.ts
+++ src/prefixes.ts
@@ -1,17 +1,14 @@
 import * as _ from 'lodash'
 var prefixes = {
-  "brt": "http://brt.basisregistraties.overheid.nl/def/top10nl#",
-  "buurt": "https://krr.triply.cc/Kadaster/cbs/id/buurt/",
-  "cbs": "https://krr.triply.cc/Kadaster/cbs/def/",
-  "dct": "http://purl.org/dc/terms/",
-  "foaf": "http://xmlns.com/foaf/0.1/",
-  "geo": "http://www.opengis.net/ont/geosparql#",
-  "graph": "https://data.pdok.nl/graph/",
-  "rce": "https://cultureelerfgoed.nl/vocab/",
-  "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-  "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-  "wijk": "https://krr.triply.cc/Kadaster/cbs/id/wijk/",
-  "xsd": "http://www.w3.org/2001/XMLSchema#"
+  "bgt": "http://bgt.basisregistraties.overheid.nl/def/bgt#",
+"brt": "http://brt.basisregistraties.overheid.nl/def/top10nl#",
+"dct": "http://purl.org/dc/terms/",
+"foaf": "http://xmlns.com/foaf/0.1/",
+"geo": "http://www.opengis.net/ont/geosparql#",
+"rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+"rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+"xsd": "http://www.w3.org/2001/XMLSchema#"
+
 }
 
 export default prefixes;
