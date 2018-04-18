60b66d68412fe1cb4e4d4dccafb6a94dcb343403
Wouter Beek <wouter@triply.cc>
2018-04-18 12:19:27 +0200

    CLEANUP: Removed deployment-specific configuration.
    These configurations will be added to alternative branches.

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import { GlobalConfig } from "../facetConfUtils";
 
 const conf: GlobalConfig = {
   endpoint: {
-    url: "https://triply.eculture.labs.vu.nl/sparql",
+    url: "SPARQL-ENDPOINT"
     token: null
   }
 };
