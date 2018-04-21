c45450100fae29d4d6f16959f021dcfeb35aefa7
Laurens Rietveld <laurens.rietveld@gmail.com>
2018-04-22 00:05:08 +0200

    install deps on build as well

--- docker/assets/build/install.sh
+++ docker/assets/build/install.sh
@@ -4,6 +4,7 @@ set -e
 adduser --disabled-login --gecos 'Facetcheck' "${FACETCHECK_USER}"
 passwd -d "${FACETCHECK_USER}"
 
+cd ${FACETCHECK__NODE_DIR} && npm install;
 
 # make sure everything in "${HOME_DIR}" is owned by the facetcheck user
 chown -R "${FACETCHECK_USER}":"${FACETCHECK_USER}" "${HOME_DIR}"/
