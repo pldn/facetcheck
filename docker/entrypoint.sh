740d57496ce532e28ee854722227da9ddfd6b718
Laurens Rietveld <laurens.rietveld@gmail.com>
2018-05-09 09:26:49 +0200

    added bundle command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,11 +9,15 @@ source ${FACETCHECK_RUNTIME_DIR}/functions
 map_uidgid
 
 case ${1} in
-  app:start|app:dev)
+  app:start|app:dev|app:bundle)
 
 
 
     case ${1} in
+      app:bundle)
+        exec_as_facetcheck npm run build;
+        cp -r ./build/assets/dist ./src/config/.bundle;
+        ;;
       app:start)
         exec_as_facetcheck npm run build;
         exec_as_facetcheck npm run start;
@@ -26,6 +30,7 @@ case ${1} in
   app:help)
     echo "Available options:"
     echo " app:start        - Starts the facetcheck server (default)"
+    echo " app:bundle       - Transpiles facetcheck and its config"
     echo " app:dev          - Starts the facetcheck server, and watches for any file changes"
     echo " app:help         - Displays the help"
     echo " [command]        - Execute the specified command, eg. bash."
