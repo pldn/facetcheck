4645b51bad3a686842074f0f8ab86dcf51d2f618
ysgorg <yysgorg@gmail.com>
2019-10-24 13:11:50 +0200

    #3499 add download-as-csv feature

--- src/containers/Nav/index.tsx
+++ src/containers/Nav/index.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import * as getClassName from "classnames";
+import { Button } from "react-toolbox/lib/button";
 
 //import own dependencies
 import {  Notifications, Svg } from "../../components";
@@ -9,18 +10,22 @@ import { Panel } from "../../containers";
 import { StateInterface, removeNotification } from "../../reducers/notifications";
 import { GlobalState } from "../../reducers";
 import {getLogo} from '../../facetConf'
+import globalConfig from '../../config/config';
 
 export interface IAppProps {
   logout?: Function;
   notifications?: StateInterface;
   removeNotification?: typeof removeNotification;
   pushState?: Function;
+  statements?:GlobalState['statements']
 }
 
 import * as styles from "./style.module.scss"
+import { downloadCsv } from "../../helpers/Csv";
 const mapStateToProps = (state: GlobalState): IAppProps =>
   ({
-    notifications: state.notifications
+    notifications: state.notifications,
+    statements:state.statements
   } as IAppProps);
 
 
@@ -29,7 +34,17 @@ const mapStateToProps = (state: GlobalState): IAppProps =>
 })
 export default //had to modify the typescript definition of the connect function to return ClassDecorator.
 //otherwise, you're not able to extend the component with any other properties than defined in the component class
-class App extends React.PureComponent<IAppProps, any> {
+class Navbar extends React.PureComponent<IAppProps, any> {
+
+  download = () => {
+    downloadCsv(
+      // get the list of statements from the state
+      this.props.statements.resourceDescriptions.toSeq().toArray(),
+      // use the page title from the config as a basis for filename
+      globalConfig.title
+    );
+  }
+
   render() {
     const {  notifications, removeNotification } = this.props;
 
@@ -43,6 +58,7 @@ class App extends React.PureComponent<IAppProps, any> {
           <a href='/'>
           <Svg src={getLogo()} className={styles.logo} />
           </a>
+          <Button className={styles.downloadButton} onClick={this.download}>Download csv</Button>
         </div>
         <Notifications closeHandler={removeNotification} notifications={notifications} />
         <Panel/>
---
+++ src/helpers/Csv.ts
@@ -0,0 +1,81 @@
+import { Quad, DataFactory } from "n3";
+import Tree from './Tree';
+import { getLabel, getWidgets } from "../reducers/statements";
+import * as json2csv from 'json2csv';
+import { saveAs } from 'file-saver';
+import {deburr, kebabCase} from 'lodash';
+
+function getValue(node: Tree, tree: Tree) {
+  const term = node.getTerm();
+  if (term.termType === 'NamedNode') return getLabel(term, tree);
+  return term.value;
+}
+
+function createDownloadFilename(downloadFilenameBase:string) {
+  if (!downloadFilenameBase) return "facetcheck.csv";
+  // deburr: e.g. Å -> A, è -> e
+  // kebabCase: e.g. "makeMe a KEBAB" -> make-me-a-kebab
+  // regex replace: remove non-alphanumeric non-dash chars, e.g. 😋 -> ''
+  return `${deburr(kebabCase(downloadFilenameBase).replace(/[^a-z0-9\-]/gi, ''))}.csv`;
+}
+
+interface Row { [key: string]: string }
+
+export function downloadCsv(descriptions: Array<[string, Array<Quad>]>, downloadFilenameBase: string) {
+
+  const rows: Row[] = [];
+
+  // Keep track of the properties we encounter.
+  // Distinguish normal properties from 'special' ones (leaflet, image source)
+  // (we need this later for specifying the order or columns)
+  const normalHeaderNames: Set<string> = new Set();
+  const specialHeaderNames: Set<string> = new Set();
+
+  for (const d of descriptions) {
+
+    // Get the resource and create a Tree and WidgetConfig for it.
+    const resource = DataFactory.namedNode(d[0]);
+    const tree = Tree.fromStatements(resource, d[1]);
+    const widgets = getWidgets(tree);
+
+    // Initialize a row for the csv, and create a cell for the resource iri.
+    const row: Row = { resource: getLabel(resource, tree) }
+
+    // The list of normal properties can be identified by its' "Properties" label.
+    const normalProperties = widgets.children.filter(c => c.label === "Properties")[0].children;
+
+    // Create a cell in the row for each property.
+    // We use the first of its' objects as value.
+    for (const child of normalProperties) {
+      if (child.values && child.values.length) {
+        row[child.label] = getValue(child.values[0], tree);
+        normalHeaderNames.add(child.label);
+      }
+    }
+
+    // Special properties are all the property lists that don't have the "Properties" label.
+    const specialProperties = widgets.children.filter(c => c.label !== "Properties");
+
+    // Create cells for each kind of the special properties.
+    // We only use one of each kind, consistently with the web interface.
+    for (const specialProperty of specialProperties) {
+      const objectTree = specialProperty.values[0];
+      const label = getLabel(objectTree.getPredicate(), tree);
+      row[label] = getValue(objectTree, tree);
+      specialHeaderNames.add(label);
+    }
+    rows.push(row);
+  }
+
+  const csvContent = json2csv.parse(rows,
+    {
+      // Specify the order of columns.
+      // Special properties include wkts, which can be very large.
+      // Therefore we want them all the way to the right.
+      fields: ['resource', ...normalHeaderNames, ...specialHeaderNames]
+    }
+  )
+  saveAs(
+    new Blob([csvContent], {type: "text/plain;charset=utf-8"}),
+    createDownloadFilename(downloadFilenameBase));
+}
